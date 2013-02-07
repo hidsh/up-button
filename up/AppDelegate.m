5178889a766784eeecf539df381413709a81096c
gnrr <hideaki.sh@gmail.com>
2013-02-08 08:35:06 +0900

    Initial Commit

---
+++ up/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  up
+//
+//  Created by g on 2/8/13.
+//  Copyright (c) 2013 tekitronix. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
